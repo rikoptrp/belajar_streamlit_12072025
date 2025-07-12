6cf8f6a29626a44dd897c3e669c6119f44a9115c
rikoptrp <rikoputrapamungkas2006@gmail.com>
2025-07-12 09:09:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 st.markdown("*Streamlit* is **really** ***cool***.")
 st.metric(label="Temperature", value="70 °F")
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
+  
